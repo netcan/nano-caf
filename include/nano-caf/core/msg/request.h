d560d2a6a89e6c03e555295772ef16484e85119d
Darwin Yuan <darwin_yuan@hotmail.com>
2020-08-08 23:09:43 +0800

    added test_request

--- include/nano-caf/core/msg/request.h
+++ include/nano-caf/core/msg/request.h
@@ -7,27 +7,63 @@
 
 #include <nano-caf/nano-caf-ns.h>
 #include <nano-caf/core/msg/atom.h>
+#include <nano-caf/util/macro_basic.h>
+#include <nano-caf/util/macro_pp_size.h>
+#include <nano-caf/util/macro_reflex_call.h>
+#include <nano-caf/util/callable_trait.h>
 
 NANO_CAF_NS_BEGIN
 
-template<typename ... Ts>
-struct typed_actor {
+namespace detail {
+   template<typename ATOM, typename SIGNATURE>
+   struct request_type {
+      request_type() = delete;
+      request_type(const request_type&) = delete;
+      request_type(request_type&&) = delete;
 
-};
-
-struct atom_1 : atom_signature {};
-struct atom_2 : atom_signature {};
-
-#define METHOD(method, signature) \
-struct method ##_atom : atom_signature { \
-   using type = auto signature; \
+      static_assert(!callable_trait<SIGNATURE>::is_variadic, "request does not support variadic arguments");
+      using return_type  = typename callable_trait<SIGNATURE>::return_type;
+      using args_type    = typename callable_trait<SIGNATURE>::decayed_args_type;
+      using pattern_type = typename callable_trait<SIGNATURE>::decayed_args_type::template prepend_type<ATOM>;
+   };
 }
 
+namespace detail {
+   template<typename T, size_t ... I>
+   auto deduce_method_atoms(std::index_sequence<I...>)
+   -> type_list<typename T::template __SeCrEtE_method<I, T>::method_type...>;
 
-struct actor_interface {
-   METHOD(a, (int) -> int);
-   METHOD(b, (int) -> void);
-};
+   template<typename T>
+   using method_atoms = decltype(detail::deduce_method_atoms<T>(std::make_index_sequence<T::total_methods>{}));
+}
+
+#define __CUB_method_name(x)      __CUB_1st   x
+#define __CUB_method_signature(x) __CUB_rest  x
+
+#define __CUB_atom_type(x) __CUB_paste(__CUB_method_name(x), _atom)
+////////////////////////////////////////////////////////////////////////////////////////////////
+#define __CUB_actor_method(n, x) \
+struct __CUB_atom_type(x); \
+template <typename T> struct __SeCrEtE_method<n, T>               \
+   : detail::request_type<__CUB_atom_type(x), auto __CUB_method_signature(x)> {                      \
+   using method_type = __CUB_atom_type(x); \
+   constexpr static auto name() -> const char* const { return __CUB_stringify(__CUB_method_name(x)); } \
+}; \
+struct __CUB_atom_type(x) : atom_signature { \
+   using type = __SeCrEtE_method<n, __SeCrEtE_tHiS_tYpe>; \
+}; \
+constexpr static __CUB_atom_type(x) __CUB_method_name(x);
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+#define __CAF_actor_interface(name, ...)                                                 \
+struct name {                                                                            \
+private:                                                                                 \
+   using __SeCrEtE_tHiS_tYpe = name;                                                     \
+public:                                                                                  \
+   template <size_t, typename> struct __SeCrEtE_method;                                  \
+   constexpr static size_t total_methods = __CUB_pp_size(__VA_ARGS__);                   \
+   __CUB_overload(__CUB_repeat_call_, __VA_ARGS__) (__CUB_actor_method, 0, __VA_ARGS__); \
+}
 
 NANO_CAF_NS_END
 
--- include/nano-caf/util/type_list.h
+++ include/nano-caf/util/type_list.h
@@ -15,6 +15,15 @@ struct type_list {
    constexpr static size_t size = 0;
    template<template<typename T> typename F>
    using transform = type_list<>;
+
+   template<typename T>
+   using append_type = type_list<T>;
+
+   template<typename T>
+   using prepend_type = type_list<T>;
+
+   template<template<typename ...> typename R>
+   using export_to = R<>;
 };
 
 template<typename H, typename ... Ts>
@@ -28,7 +37,31 @@ struct type_list<H, Ts...> {
 
    template<template<typename T> typename F>
    constexpr static bool pred = (F<Ts>::value && ... && F<H>::value);
+
+   template<typename T>
+   using prepend_type = type_list<T, H, Ts...>;
+
+   template<typename T>
+   using append_type = type_list<H, Ts..., T>;
+
+   template<template<typename ...> typename R>
+   using export_to = R<H, Ts...>;
 };
+
+namespace detail {
+   template<typename T, typename ... Ts>
+   struct type_list_exists {
+      constexpr static bool value = false;
+   };
+
+   template<typename T, typename H, typename ... Ts>
+   struct type_list_exists<T, H, Ts...> {
+      constexpr static bool value = std::is_same_v<T, H> || type_list_exists<T, Ts...>::value;
+   };
+}
+
+template<typename T, typename TL>
+constexpr bool type_list_exists = TL::template prepend_type<T>::template export_to<detail::type_list_exists>::value;
 
 NANO_CAF_NS_END
 
