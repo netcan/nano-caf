b0f28df5e02f842ebd4e421d81edd5675d538fd8
Darwin Yuan <darwin_yuan@hotmail.com>
2020-08-23 19:12:00 +0800

    make duration could be represented by chrono

--- tests/ft/test_actor_system.cpp
+++ tests/ft/test_actor_system.cpp
@@ -21,7 +21,7 @@ struct my_actor : actor {
           case test_message::type_id: {
              std::cout << "start timer : " << test_message::type_id << std::endl;
              auto status = every(1s, [&]{
-                std::cout << "timeout : " << timeout_msg::type_id << std::endl;
+                std::cout << "timeout "  << std::endl;
                 if(times++ >= 5) {
                    std::cout << "ready to stop" << std::endl;
                    stop_timer(timer_id_);
